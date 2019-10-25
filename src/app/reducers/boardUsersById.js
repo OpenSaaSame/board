bbef3001fdf88847e824232baf3c2a77fb251b2b
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2019-10-25 17:03:52 +0200

    Optimistic updates for sharing dialog

--- src/app/reducers/boardUsersById.js
+++ src/app/reducers/boardUsersById.js
@@ -9,11 +9,41 @@ const boardUsersById = (state = {}, action) => {
             users: [{"_1": userId, "_2": "SignedAdmin"}],
           }
         };
-      }    
+      }
     case "DELETE_BOARD": {
       const { boardId } = action.payload;
       const { [boardId]: deletedBoard, ...restOfBoards } = state;
       return restOfBoards;
+    }
+    case "ADD_USER": {
+      const { boardId, newUser } = action.payload;
+      return {
+        ...state,
+        [boardId]: {
+          ...state[boardId],
+          users: [{"_1": newUser, "_2": "Read"}].concat(state[boardId].users)
+        }
+      }
+    }
+    case "REMOVE_USER": {
+      const { boardId, user } = action.payload;
+      return {
+        ...state,
+        [boardId]: {
+          ...state[boardId],
+          users: state[boardId].users.filter(u => u._1 !== user)
+        }
+      }
+    }
+    case "CHANGE_PERMISSIONS": {
+      const { boardId, user, access } = action.payload;
+      return {
+        ...state,
+        [boardId]: {
+          ...state[boardId],
+          users: state[boardId].users.map(u => u._1 !== user ? u : {...u, _2: access})
+        }
+      }
     }
     case "SUCCEED_READ": {
       return action.payload.boardUsersById;
