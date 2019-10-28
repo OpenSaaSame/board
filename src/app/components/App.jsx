da32bca0db6a0b87a4ad63d087a85412369cd67d
Bernhard Elsner <bernhard.elsner@digitalasset.com>
2019-10-28 09:17:53 +0100

    Spinner

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -5,20 +5,29 @@ import { connect } from "react-redux";
 import Home from "./Home/Home";
 import BoardContainer from "./Board/BoardContainer";
 import "./App.scss";
+import { MoonLoader } from "react-spinners";
 
-const App = ({ user }) => {
+const App = ({ user, ledger }) => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/b/:boardId" component={BoardContainer} />
-      <Redirect to="/" />
-    </Switch>
+    <div className="app">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/b/:boardId" component={BoardContainer} />
+        <Redirect to="/" />
+      </Switch>
+      <div className='spinner'>
+        <MoonLoader
+          color={'rgba(0,0,0,0.8)'}
+          loading={user && (ledger.read.inProgress || ledger.read.queued || ledger.write.queue.length > 0 || ledger.write.inProgress) && true}
+        />
+      </div> 
+    </div>
   );
 };
 
 App.propTypes = { user: PropTypes.object };
 
-const mapStateToProps = state => ({ user: state.user });
+const mapStateToProps = state => ({ user: state.user, ledger: state.ledger });
 
 // Use withRouter to prevent strange glitch where other components
 // lower down in the component tree wouldn't update from URL changes
