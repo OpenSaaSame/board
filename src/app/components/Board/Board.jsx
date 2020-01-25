14700c671732da76e517094466ac36e7fd7494dc
Jeremy Marc <jeremy.marc@me.com>
2020-01-24 19:15:06 -0600

    improve mobile style

--- src/app/components/Board/Board.jsx
+++ src/app/components/Board/Board.jsx
@@ -138,7 +138,7 @@ class Board extends Component {
     return (
       <>
         <div className={classnames("board", boardColor)}>
-          <Title>{boardTitle} | Danban</Title>
+          <Title>{boardTitle} | OpenWork</Title>
           <Header />
           <BoardHeader hasAdmin={this.props.hasAdmin} />
           {/* eslint-disable jsx-a11y/no-static-element-interactions */}
