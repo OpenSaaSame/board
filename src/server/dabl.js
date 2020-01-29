b00f41047463af1fcecf6f7a2615aa4958277068
Daniel Feichtinger <daniel@digitalasset.com>
2020-01-29 15:21:03 -0800

    Sleep after creating the party and before reading it back from the ledger

--- src/server/dabl.js
+++ src/server/dabl.js
@@ -203,6 +203,7 @@ const dabl = () => {
         }
     }
 
+
     const partyTemplate = {
         "moduleName": "DABL.Ledger.V2",
         "entityName": "LedgerParty"
@@ -210,6 +211,9 @@ const dabl = () => {
 
     const getDABLUser = async user => {
         let party_ = null;
+        const sleep = (milliseconds) => {
+            return new Promise(resolve => setTimeout(resolve, milliseconds))
+        };        
 
         const userPartyInner = async () => {
             try {
@@ -217,6 +221,11 @@ const dabl = () => {
                 const createCb = async () => {
                     try {
                         await createUser(user);
+                        
+                        // Required as there's a delay between the above call returning
+                        // and when the part contract appears on the ledger...
+                        await sleep(2000);
+
                         const response = await search(
                             dataURL,
                             adminJwt,
