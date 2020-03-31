a11801782827c335663501f5193c01b684c213b9
Daniel Feichtinger <daniel@digitalasset.com>
2020-03-30 17:29:01 -0700

    Use hash paths for DABL support

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";   
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import rootReducer from "./reducers";
 import App from "./components/App";
 import persistMiddleware from "./middleware/persistMiddleware";
@@ -34,9 +34,9 @@ poll();
 function Appl() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   );
 }
