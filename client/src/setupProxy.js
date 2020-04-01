c7ae61681f1b648c9a4c5d80844a5f0bbeccedb0
Daniel Feichtinger <daniel@digitalasset.com>
2020-04-01 16:53:07 -0700

    Tidy up proxy setup

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,6 @@ module.exports = function(app) {
   app.use(
     '/v1',
     createProxyMiddleware({
-      // target: 'https://a05bafae-8ea2-4847-9d1a-a11573732e04.mock.pstmn.io/',
       target: 'http://localhost:7575/',
       changeOrigin: true,
       logLevel: 'debug'
